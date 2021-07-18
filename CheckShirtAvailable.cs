cd56506939db0d8d0ba486509e42405cf53c760e
Afa Cheng <afa@afa.moe>
2021-07-18 18:40:36 +0800

    done

--- CheckShirtAvailable.cs
+++ CheckShirtAvailable.cs
@@ -1,13 +1,12 @@
-using System;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 using HtmlAgilityPack;
-using System.Collections.Generic;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace iamnook
 {
@@ -15,22 +14,34 @@ namespace iamnook
     {
         private const string PRODUCT_URL = @"https://store-jp.nintendo.com/list/goods/fashion/NSJ_8_A2AAP.html";
 
+        private const string SETTINGS_SIZE_IN_INTEREST = @"SIZE_IN_INTEREST";
+        private const string SETTINGS_SENDGRID_API_KEY = @"SENDGRID_API_KEY";
+        private const string SETTINGS_SENDGRID_RECIPIENT = @"MSG_RECEIPIENT";
+
         // every 24/6 hour, i.e., 0,6,12,18 o'clock
         [FunctionName("CheckShirtAvailable")]
-        public static async Task Run([TimerTrigger("0 0 */6 * * *")] TimerInfo timer, ILogger log)
+        public static async Task Run(
+            [TimerTrigger("0 0 */6 * * *",
+#if DEBUG
+            RunOnStartup = true
+#endif
+            )] TimerInfo timer,
+            ILogger log)
         {
             log.LogInformation($"Start checking Nook's shirt at: {DateTime.Now}");
 
-            var stockInformation = LoadStockInformation();
-            stockInformation.Remove("130");
+            var stockInformation = LoadStockInformation(log);
 
-            if (stockInformation.Any(pair => pair.Value))
+            var sizeInInterestStr = Environment.GetEnvironmentVariable(SETTINGS_SIZE_IN_INTEREST);
+            var sizeInInterest = sizeInInterestStr?.Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            if (sizeInInterest?.Any(size => stockInformation.ContainsKey(size) && stockInformation[size]) ?? false)
             {
-                await SendEmailShirtAvailable(stockInformation);
+                await SendEmailShirtAvailable(stockInformation, log);
             }
         }
 
-        private static Dictionary<string, bool> LoadStockInformation()
+        private static Dictionary<string, bool> LoadStockInformation(ILogger log)
         {
             var stockInformation = new Dictionary<string, bool>();
 
@@ -44,13 +55,18 @@ namespace iamnook
                 return stockInformation;
             }
 
-            var xpathForSize = @".//li[contains(@class, ""productDetail--type__item"")]//span[contains(@class, ""productDetail--type__name"")]";
-            var xpathForPrice = @".//li[contains(@class, ""productDetail--type__item"")]//span[contains(@class, ""productDetail--type__price"")]";
+            var xpathForSize = @".//span[contains(@class, ""productDetail--type__name"")]";
+            var xpathForPrice = @".//span[contains(@class, ""productDetail--type__price"")]";
 
             foreach (var li in lis)
             {
                 var sizeElement = li.SelectSingleNode(xpathForSize);
                 var priceElement = li.SelectSingleNode(xpathForPrice);
+
+                if (sizeElement == null || priceElement == null)
+                {
+                    continue;
+                }
 
                 var size = sizeElement.InnerText;
                 var inStock = !priceElement.InnerHtml.Contains("品切れ");
@@ -61,11 +77,23 @@ namespace iamnook
             return stockInformation;
         }
 
-        private static async Task SendEmailShirtAvailable(Dictionary<string, bool> stockInformation)
+        private static async Task SendEmailShirtAvailable(Dictionary<string, bool> stockInformation, ILogger log)
         {
-            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-            var recipient = Environment.GetEnvironmentVariable("MSG_RECEIPIENT");
-            var recipients = recipient.Split(';');
+            var apiKey = Environment.GetEnvironmentVariable(SETTINGS_SENDGRID_API_KEY);
+            var recipient = Environment.GetEnvironmentVariable(SETTINGS_SENDGRID_RECIPIENT);
+            var recipients = recipient?.Split(';', StringSplitOptions.RemoveEmptyEntries) ?? new string[0];
+
+            if (apiKey == null)
+            {
+                log.LogError("API key for SendGrid is not found. Not sending the mail.");
+                return;
+            }
+
+            if (recipients.Length == 0)
+            {
+                log.LogError("No recipient provided. Not sending the mail.");
+                return;
+            }
 
             var client = new SendGridClient(apiKey);
 
@@ -74,11 +102,9 @@ namespace iamnook
             var msg = new SendGridMessage()
             {
                 From = new EmailAddress("no-reply@afa.moe", "Nook's Shirt Notification Service"),
-                Subject = "Nook's shirts are restocked!",
-                PlainTextContent = "and easy to do anywhere, even with C#",
+                Subject = "Nook's shirts restocked!",
                 HtmlContent = $@"
                 {stockDescription}
-                <p></p>
                 <p>Visit this <a href=""https://store-jp.nintendo.com/list/goods/fashion/NSJ_8_A2AAP.html"">link</a> to buy!</p>"
             };
 
@@ -86,7 +112,15 @@ namespace iamnook
             {
                 msg.AddTo(new EmailAddress(recipient, recipient));
             }
-            await client.SendEmailAsync(msg);
+            var response = await client.SendEmailAsync(msg);
+            if (response.IsSuccessStatusCode)
+            {
+                log.LogInformation("Successfully sent the mail to recipients!");
+            }
+            else
+            {
+                log.LogError($"Failed to sent the mail to recipients: {await response.Body.ReadAsStringAsync()}");
+            }
         }
     }
 }
